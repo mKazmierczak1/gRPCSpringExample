c050f48fec74feb2caa2bb66305599e68bb61a7e
Michał Kaźmierczak <mkazmierczak112@gmail.com>
2023-03-11 17:21:38 +0100

    Initial commit

---
+++ src/main/java/org/example/gRPCSpringExample/GRpcSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.gRPCSpringExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GRpcSpringExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gRPCSpringExample/GRpcSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gRPCSpringExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
